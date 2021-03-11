2f89eac11d3bfbc38001357cc14f2403ede94702
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-03-11 22:18:49 +1100

    styled-components

---
+++ src/App/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  display: flex;
+`;
+
+const Header = () => (
+    <Layout>
+        31
+    </Layout>
+);
+
+export default Header;
