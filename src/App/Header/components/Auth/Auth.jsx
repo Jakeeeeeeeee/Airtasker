1e4a367b03f612fc8c3f87353cd188585155619b
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-04-01 02:48:24 +1100

    feat(SignUpModal): introduce the state for SIgnUpModal

--- src/App/Header/components/Auth/Auth.jsx
+++ src/App/Header/components/Auth/Auth.jsx
@@ -1,24 +1,62 @@
 import styled from 'styled-components';
 import HeaderItem from '../../../../components/HeaderItem';
 import Button from '../../../../components/Button';
+import React from 'react';
+import SignUpModal from './components/SignUpModal';
 
 const Layout = styled.div`
   display: flex;
   align-items: center;
 `;
 
-const Auth = () => (
-  <Layout>
-    <HeaderItem>
-      <Button variant='nacked'>Sign Up</Button>
-    </HeaderItem>
-    <HeaderItem>
-      <Button variant='nacked'>Login in</Button>
-    </HeaderItem>
-    <HeaderItem>
-      <Button variant='secondary'>Become a Tasker</Button>
-    </HeaderItem>
-  </Layout>
-);
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentModal: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+
+    this.setState({
+      currentModal: true,
+    });
+  }
+
+  handleCloseModal() {
+    this.setState({
+      currentModal: false,
+    });
+  }
+
+  render() {
+    const { currentModal } = this.state;
+    
+    return (
+      <>
+        <Layout>
+          <HeaderItem>
+            <Button size="sm" variant='nacked' onClick={this.handleClick}>
+              Sign Up
+            </Button>
+          </HeaderItem>
+          <HeaderItem>
+            <Button size="sm" variant='nacked'>Login in</Button>
+          </HeaderItem>
+          <HeaderItem>
+            <Button size="sm" variant='secondary'>Become a Tasker</Button>
+          </HeaderItem>
+        </Layout>
+        {currentModal && <SignUpModal onClose={this.handleCloseModal} />}
+      </>
+    )
+  }
+}
 
 export default Auth;
