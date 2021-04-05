1a89a61c0b733115eab490980d64a55e7df340d5
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-04-06 04:29:39 +1000

    feat(MainBody):introduce the Mainbody component

---
+++ src/App/MainBody/MainBody.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  display: flex;
+`;
+
+const MainBody = () => {
+  return (
+    <Content>
+      123
+    </Content>
+  );
+};
+
+export default MainBody;
