49a1a2f0f8eaee749172e99d98d3caf7eef50c2f
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-04-06 04:26:39 +1000

    feat(FormItem):introduce the reusable FormItem component

---
+++ src/components/FormItem/FormItem.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Label from '../Label';
+
+const Layout = styled.div`
+  & ~ & {
+    margin-top: 16px;
+  }
+`;
+
+const FormItem = ({
+  label,
+  children,
+}) =>(
+  <Layout>
+    {label && (<Label>{label}</Label>)}
+    {children}
+  </Layout>
+);
+
+export default FormItem;
