2d73b8236469e3b8b1ac823ecf7338f0b2108eec
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-04-18 20:45:56 +1000

    feat(Navigation): introduce the dropdown to Navigation

--- src/App/Header/components/Navigation/Navigation.jsx
+++ src/App/Header/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import HeaderItem from '../../../../components/HeaderItem';
 import Button from '../../../../components/Button';
+import React from 'react';
 
 const Layout = styled.div`
   flex: 1;
@@ -16,15 +17,55 @@ const Link = styled.a`
 
 const Toggler = styled.div`
   cursor: pointer;
+  position: relative;
 `;
 
-const Navigation = () => (
+const DropDown = styled.div`
+  position: absolute;
+  top: 40px;
+  border: 1px solid black;
+  width: 150px;
+  background-color: white;
+  padding: 12px;
+`;
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDropdownShown: false,
+    };
+
+  }
+
+  handleShowDropdown() {
+    this.setState({
+      isDropdownShown: true,
+    });
+  }
+
+  handleCloseDropdown() {
+    this.setState({
+      isDropdownShown: false,
+    });
+  }
+
+  render() {
+    const { isDropdownShown } = this.state;
+  return (
+    <>
     <Layout>
       <HeaderItem>
         <Button variant='primary'>Post a task</Button>
       </HeaderItem>
-      <HeaderItem highlight as={Toggler}>
-          Categories
+      <HeaderItem 
+        highlight as={Toggler}
+        onMouseEnter={() => this.handleShowDropdown()}
+        onMouseLeave={() => this.handleCloseDropdown()}
+      >
+        Categories
+        {isDropdownShown && <DropDown>this is a dropdown</DropDown>}
       </HeaderItem>
       <HeaderItem highlight as={Link} href="/tasks">
         Browse tasks
@@ -33,6 +74,9 @@ const Navigation = () => (
         How it works
       </HeaderItem>
     </Layout>
-);
+    </>
+  );
+}
+}
 
 export default Navigation;
